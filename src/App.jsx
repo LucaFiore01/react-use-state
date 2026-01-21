1a8329f393c6b397202ef6509865fcd3ce80747f
Luca Fiore <lf44551@gmail.com>
2026-01-21 15:01:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import languages from '../data/languages'
+
+function App() {
+  return (
+    <main className="app">
+      <header className="app__header">
+        <h1>Linguaggi del web</h1>
+        <p>Scegli un linguaggio dalla lista.</p>
+      </header>
+
+      <div className="app__buttons">
+        {languages.map(({ id, title }) => (
+          <button key={id} className="language-button">
+            {title}
+          </button>
+        ))}
+      </div>
+    </main>
+  )
+}
+
+export default App
